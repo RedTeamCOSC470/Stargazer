9d8c87ffc2606682873314611e8588207e566ff0
Rob Grmek <rgrmek@gmail.com>
2009-11-15 15:11:07 -0800

    Added view for listing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
   def new  
-      @user = User.new  
+    @user = User.new  
   end  
     
   def create  
     @user = User.new(params[:user])  
     if @user.save  
       flash[:notice] = "Registration successful."  
-      redirect_to root_url  
+      redirect_to users_path
     else  
       render :action => 'new'  
     end  
@@ -26,4 +26,8 @@ class UsersController < ApplicationController
       render :action => 'edit'  
     end  
   end  
+  
+  def index
+  	@users = User.all
+  end
 end
