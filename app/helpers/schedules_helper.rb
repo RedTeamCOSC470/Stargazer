a5c4a8eb8f9645f3c5908149e3fc7500d46dfb00
Rob Grmek <rgrmek@gmail.com>
2009-11-28 19:03:00 -0800

    fully fixed schedule forms

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,2 +1,6 @@
 module SchedulesHelper
+  def get_default_date
+    { :day => params[:date]['get(3i)'], :month => params[:date]['get(2i)'], 
+      :year => params[:date]['get(1i)'] } rescue Date.current 
+  end
 end
