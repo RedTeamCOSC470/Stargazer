99d515018fbe435c9c5fc50818b47876ec7a6a51
unknown <Rob@.(none)>
2010-02-17 13:39:30 -0800

    fixed searching with schedules

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -13,17 +13,10 @@ class SchedulesController < ApplicationController
   # GET /schedules
   # GET /schedules.xml
   def index
-    
-    # check to see if user has selected view all button or not
-    if params[:commit] == "Go"
-      search_date = "#{params[:date]['get(1i)']}-#{params[:date]['get(2i)']}-#{params[:date]['get(3i)']}" rescue nil
-    else
-      search_date = nil
-    end
 
     # find all schedules by that date and add pagination
-    @schedules = Schedule.search_by_date(search_date).order_by_recent.paginate :page => params[:page],
-                                                                               :per_page => 5
+    @schedules = Schedule.paginate :page => params[:page], :per_page => 5, :order => "start_time",
+                                   :conditions => ['start_time LIKE ?', "%#{params[:search]}%"]
 
     respond_to do |format|
       format.html # index.html.erb
@@ -40,7 +33,7 @@ class SchedulesController < ApplicationController
   # GET /schedules/1
   # GET /schedules/1.xml
   def show
-    
+
     # any user can see any schedule
     @schedule = Schedule.find(params[:id])
 
@@ -54,7 +47,7 @@ class SchedulesController < ApplicationController
   # GET /schedules/new
   # GET /schedules/new.xml
   def new
-    
+
     # must remember which user built the schedule
     @schedule = @current_user.schedules.build
 
@@ -66,7 +59,7 @@ class SchedulesController < ApplicationController
 
   # GET /schedules/1/edit
   def edit
-    
+
     # check to see if the current user is an admin
     # otherwise, the user must own that schedule in order to access the page
     is_user_admin?
@@ -75,7 +68,7 @@ class SchedulesController < ApplicationController
   # POST /schedules
   # POST /schedules.xml
   def create
-    
+
     # must remember which user built the schedule
     @schedule = @current_user.schedules.build(params[:schedule])
 
@@ -94,7 +87,7 @@ class SchedulesController < ApplicationController
   # PUT /schedules/1
   # PUT /schedules/1.xml
   def update
-    
+
     # check to see if the current user is an admin
     # otherwise, the user must own that schedule in order to edit it
     is_user_admin?
@@ -114,7 +107,7 @@ class SchedulesController < ApplicationController
   # DELETE /schedules/1
   # DELETE /schedules/1.xml
   def destroy
-    
+
     # check to see if the current user is an admin
     # otherwise, the user must own that schedule in order to delete it
     is_user_admin?
@@ -125,7 +118,7 @@ class SchedulesController < ApplicationController
       format.xml  { head :ok }
     end
   end
-  
+
   private
   def is_user_admin?
     # check to see if the current user is an admin
