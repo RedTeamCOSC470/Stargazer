dd88fe7dac6590664765f3071629a20a8a11d84b
unknown <Rob@.(none)>
2010-02-17 14:08:50 -0800

    added park action on controller

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -10,11 +10,21 @@ class InfoController < ApplicationController
   
   # require authentication on all pages
   before_filter :require_user
+
+  # allow only admins to park the telescope
+  before_filter :authorize, :except => [:about, :help]
   
   def about
   end
 
   def help
+  end
+
+  # for parking the telescope
+  def park
+
+    # return to the previous page
+    redirect_to :back
   end
 
 end
