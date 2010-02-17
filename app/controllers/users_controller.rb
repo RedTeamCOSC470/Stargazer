e41ebb6ac54df6e519966268c930e66356f12f72
unknown <Rob@.(none)>
2010-02-17 13:17:05 -0800

    added pagination and search to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,9 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.xml
   def index
-  	@users = User.all
+    # find all users either by searching or all; also add pagiation
+  	@users = User.paginate :page => params[:page], :per_page => 5, :order => 'username',
+                           :conditions => ['username LIKE ?', "%#{params[:search]}%"]
   end
 
   # GET /users/1
